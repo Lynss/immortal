1a7f9fa3e969bffc047946493ce64dfb89c37d39
ly <ly1169134156@163.com>
2017-08-23 21:28:15 +0800

    master:Initial commit

---
+++ src/main/java/com/ly/immortal/ImmortalApplication.java
@@ -0,0 +1,12 @@
+package com.ly.immortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmortalApplication.class, args);
+	}
+}
