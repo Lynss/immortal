e3fbcc9ed3578b693e21adff8b463fec46277412
龙云 <ly1169134156@163.com>
2017-10-13 17:04:45 +0800

    龙云@171013 测试devtools以及thymeleaf模板

--- src/main/kotlin/com/ly/immortal/controller/TestController.kt
+++ src/main/kotlin/com/ly/immortal/controller/TestController.kt
@@ -1,10 +1,16 @@
 package com.ly.immortal.controller
 
+import com.ly.immortal.domain.TestThymeleaf
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.ModelAndView
 
 @Controller
 class TestController {
     @GetMapping(value = "/index")
-    fun home() = "page/index.html"
+    fun home():ModelAndView{
+        val modelAndView = ModelAndView("test")
+        modelAndView.addObject("test", TestThymeleaf("1","2"))
+        return modelAndView
+    }
 }
---
+++ src/main/kotlin/com/ly/immortal/domain/pojos.kt
@@ -0,0 +1,3 @@
+package com.ly.immortal.domain
+
+data class TestThymeleaf(val arg1: String, val arg2: String)
