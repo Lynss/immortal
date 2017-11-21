288006b89b92615dc0fe8bc40b8c1dfb9eec65a5
龙云 <ly1169134156@163.com>
2017-11-21 16:54:19 +0800

    龙云@171013 测试kotlin in action 的注解

---
+++ src/main/kotlin/com/ly/immortal/Annotations.kt
@@ -0,0 +1,13 @@
+package com.ly.immortal
+
+/**
+ *@author ly
+ */
+
+@Target(AnnotationTarget.ANNOTATION_CLASS)
+annotation class MyBindingAnnotation
+
+@MyBindingAnnotation
+annotation class Mybinding
+
+
